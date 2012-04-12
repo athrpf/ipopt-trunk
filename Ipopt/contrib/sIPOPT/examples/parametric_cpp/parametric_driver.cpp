36a9eaf30aede343744158527f67abc6fe3561d9
Arne Graf <arne.graf@rwth-aachen.de>
2012-04-12 13:56:51 +0200

    changed parametric_driver

--- Ipopt/contrib/sIPOPT/examples/parametric_cpp/parametric_driver.cpp
+++ Ipopt/contrib/sIPOPT/examples/parametric_cpp/parametric_driver.cpp
@@ -13,6 +13,7 @@
 #include "SensRegOp.hpp"
 #include "IpOrigIpoptNLP.hpp"
 #include "IpDenseVector.hpp"
+#include "SensPerturbInterface.hpp"
 
 
 int main(int argv, char**argc)
@@ -44,19 +45,7 @@ int main(int argv, char**argc)
 
   retval = app_ipopt->OptimizeTNLP(sens_tnlp);
 
-  SmartPtr<const IteratesVector> curr = app_ipopt->IpoptDataObject()->curr();
-  SmartPtr<const Vector> x = curr->x();
-  SmartPtr<const Vector> y_c = curr->y_c();
-  SmartPtr<const Vector> y_d = curr->y_d();
-
-  // Get the parameter sensitivity matrices
-  SmartPtr<IpoptNLP> ipopt_nlp = app_ipopt->IpoptNLPObject();
-  SmartPtr<const Matrix> opt_jac_c_p = (dynamic_cast<OrigIpoptNLP*>(GetRawPtr(ipopt_nlp)))->jac_c_p(*x);
-  SmartPtr<const Matrix> opt_jac_d_p = (dynamic_cast<OrigIpoptNLP*>(GetRawPtr(ipopt_nlp)))->jac_d_p(*x);
-  SmartPtr<const Matrix> opt_h_p = (dynamic_cast<OrigIpoptNLP*>(GetRawPtr(ipopt_nlp)))->h_p(*x, 1.0, *y_c, *y_d);
-  opt_jac_c_p->Print(*app_ipopt->Jnlst(), J_INSUPPRESSIBLE, J_DBG, "opt_jac_c_p");
-  opt_jac_d_p->Print(*app_ipopt->Jnlst(), J_INSUPPRESSIBLE, J_DBG, "opt_jac_d_p");
-  opt_h_p->Print(*app_ipopt->Jnlst(), J_INSUPPRESSIBLE, J_DBG, "opt_h_p");
+  SensPerturbInterface sensClass (app_ipopt);
 
   // Set up the perturbed parameters
   SmartPtr<const DenseVector> p0 = dynamic_cast<const DenseVector*>(GetRawPtr(ipopt_nlp->p()));
@@ -64,30 +53,12 @@ int main(int argv, char**argc)
   Number* dp_ptr = dp->Values();
   dp_ptr[0] = -0.5;
   dp_ptr[1] = 0.0;
+  SmartPtr<IteratesVector> rhs = sensClass.getParaSensMatrix(dp_ptr);
 
   // Get the (factorized) KKT matrix
-  SmartPtr<IpoptAlgorithm> alg = app_ipopt->AlgorithmObject();
-  SmartPtr<PDSearchDirCalculator> pd_search;
-  pd_search = dynamic_cast<PDSearchDirCalculator*>(GetRawPtr(alg->SearchDirCalc()));
-  SmartPtr<PDSystemSolver> pd_solver_ = pd_search->PDSolver();
-
-  // Set up RHS and LHS for solve
-  SmartPtr<IteratesVector> rhs = app_ipopt->IpoptDataObject()->curr()->MakeNewIteratesVector();
-  rhs->Set(0.0);
-  SmartPtr<Vector> rhs_x = x->MakeNew();
-  opt_h_p->MultVector(1.0, *dp, 0.0, *rhs_x);
-  rhs->Set_x_NonConst(*rhs_x);
-  SmartPtr<Vector> rhs_c = y_c->MakeNew();
-  opt_jac_c_p->MultVector(1.0, *dp, 0.0, *rhs_c);
-  rhs->Set_y_c_NonConst(*rhs_c);
-  SmartPtr<Vector> rhs_d = y_d->MakeNew();
-  opt_jac_d_p->MultVector(1.0, *dp, 0.0, *rhs_d);
-  rhs->Set_y_d_NonConst(*rhs_d);
-
+  SmartPtr<PDSystemSolver> pd_solver_ = sensClass.getKKTmatrix();
   SmartPtr<IteratesVector> lhs = rhs->MakeNewIteratesVector();
+  SmartPtr<const IteratesVector> curr = app_ipopt->IpoptDataObject()->curr();
 
-  pd_solver_->Solve(-1.0, 0.0, *rhs, *lhs, false, false);
-
-  lhs->Axpy(1.0, *curr);
-  lhs->Print(*app_ipopt->Jnlst(), J_INSUPPRESSIBLE, J_DBG, "perturbed_x");
+  sensClass.solveSens(rhs, pd_solver_, curr);
 }
